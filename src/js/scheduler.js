970b7f4eb1bfe18462148c0f2426d1bb00a59690
Alex Vernacchia <avernacchia@salesforce.com>
2015-01-08 18:18:11 +0000

    getting it showing on page by any means necessary

--- src/js/scheduler.js
+++ src/js/scheduler.js
@@ -2,78 +2,96 @@
 
     'use strict';
 
+        var schedule = [];
+        var hoursOfDay = [];
+        var daysOfWeek = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
+        var hours = _.range( 24 );
+
+        _.forEach( hours, function( hour ) {
+            hoursOfDay.push({
+                hourOfDay: hour
+            })
+        });
+
+        _.forEach( daysOfWeek, function( day ) {
+            schedule.push({
+                name: day
+                , hours: _.clone( hoursOfDay )
+            });
+        })
+
     angular
         .module( 'sdc.scheduler', [] )
-        .controller('SdcSchedulerController', [ '$scope', function( $scope ) {
-            var schedule = [];
-            var hoursOfDay = [];
-            var daysOfWeek = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
-            var hours = _.range( 24 );
-
-            _.forEach( hours, function( hour ) {
-                hoursOfDay.push({
-                    hourOfDay: hour
-                })
-            });
-
-            _.forEach( daysOfWeek, function( day ) {
-                schedule.push({
-                    name: day
-                    , hours: hoursOfDay
-                });
-            })
-
-            $scope.schedule       = { days: schedule };
-            $scope.staticTimezone = true;
-        }])
+        // .controller('SdcSchedulerController', [ function() {
+        //     var schedule = [];
+        //     var hoursOfDay = [];
+        //     var daysOfWeek = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
+        //     var hours = _.range( 24 );
+        //
+        //     _.forEach( hours, function( hour ) {
+        //         hoursOfDay.push({
+        //             hourOfDay: hour
+        //         })
+        //     });
+        //
+        //     _.forEach( daysOfWeek, function( day ) {
+        //         schedule.push({
+        //             name: day
+        //             , hours: hoursOfDay
+        //         });
+        //     })
+        //
+        //     this.schedule       = { days: schedule };
+        //     this.staticTimezone = true;
+        // }])
         .directive('sdcScheduler', [
             function () {
                 return {
                     restrict: 'A'
-                    , controller:'SdcSchedulerController'
+                    // , controller:'SdcSchedulerController'
+                    // , controllerAs: 'sdcScheduler'
                     , transclude: false
-                    , scope: {}
                     , link: function (scope) {
 
-                        scope.scheduleModel = scope.schedule;
+                        scope.scheduleModel = { days: schedule };
 
                         scope.hoursObjTemplate = _.map(_.range(24), function (val) {
                             return { hour: val };
                         });
 
-                        // scope.isMouseDown = false;
+                        scope.isMouseDown = false;
 
-                        // scope.setActiveStateForAllHoursInDay = function (state, day) {
-                        //     angular.forEach(day.hours, function (hour) {
-                        //         hour.active = state;
-                        //     });
-                        // };
-                        //
-                        // scope.setAllHoursForDayActive = angular.bind(scope, scope.setActiveStateForAllHoursInDay, true);
-                        // scope.setAllHoursForDayInactive = angular.bind(scope, scope.setActiveStateForAllHoursInDay, false);
-                        //
-                        // scope.onMouseOver = function (hour) {
-                        //     if (scope.isMouseDown) {
-                        //         hour.active = scope.newActiveState;
-                        //     }
-                        // };
-                        //
-                        // scope.onMouseDown = function (hour) {
-                        //     hour.active = hour.active ? !hour.active : true;
-                        //     scope.isMouseDown = true;
-                        //     scope.newActiveState = hour.active;
-                        // };
-                        //
-                        // scope.onMouseUp = function () {
-                        //     scope.isMouseDown = false;
-                        //     if (scope.validate) {
-                        //         scope.validate();
-                        //     }
-                        // };
-                        //
-                        // scope.onMouseLeave = function () {
-                        //     scope.isMouseDown = false;
-                        // };
+                        scope.setActiveStateForAllHoursInDay = function (state, day) {
+                            angular.forEach(day.hours, function (hour) {
+                                hour.active = state;
+                            });
+                        };
+
+                        scope.setAllHoursForDayActive = angular.bind(scope, scope.setActiveStateForAllHoursInDay, true);
+                        scope.setAllHoursForDayInactive = angular.bind(scope, scope.setActiveStateForAllHoursInDay, false);
+
+                        scope.onMouseOver = function (hour) {
+                            if (scope.isMouseDown) {
+                                hour.active = scope.newActiveState;
+                            }
+                        };
+
+                        scope.onMouseDown = function (hour) {
+                            hour.active = hour.active ? !hour.active : true;
+                            scope.isMouseDown = true;
+                            scope.newActiveState = hour.active;
+                        };
+
+                        scope.onMouseUp = function () {
+                            scope.isMouseDown = false;
+                            if (scope.validate) {
+                                scope.validate();
+                            }
+                        };
+
+                        scope.onMouseLeave = function () {
+                            scope.isMouseDown = false;
+                        };
                     }
                 };
             }
